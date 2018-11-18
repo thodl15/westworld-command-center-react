13cc018a48190ed1a0d99d64eb7230a73535dfe7
DRooke <thodl15@gmail.com>
2018-11-18 15:28:43 -0500

    Updated the map logic to generate areas after waiting for the host list.

--- src/components/Area.js
+++ src/components/Area.js
@@ -3,12 +3,11 @@ import React from 'react';
 // Westworld Imports:
 import HostList from './HostList';
 
-const Area = () => (
+const Area = (props) => (
 
-  <div className='area' id={/* how do we get this thing styled right? See Stylesheets and Readme */}>
-    <h3 className='labels'>{/* Don't just pass in the name from the data...clean that thing up */}</h3>
+  <div className='area' id={props.id}>
+    <h3 className='labels'>{"IPSUM LOREM"}</h3>
 
-    {/* See Checkpoint 1 item 2 in the Readme for a clue as to what goes here */}
     <HostList/>
 
   </div>
@@ -17,6 +16,7 @@ const Area = () => (
 
 Area.propTypes = {
   hosts: function(props, propName, componentName){
+    // console.log(props);
     if(props.hosts.length > props.limit){
       throw Error(
         `HEY!! You got too many hosts in ${props.name}. The limit for that area is ${props.limit}. You gotta fix that!`
--- src/components/MapLogic.js
+++ src/components/MapLogic.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 // Westworld Imports
 import WestworldMap from './WestworldMap';
+import Area from './Area';
 
 class MapLogic extends React.Component {
     // We would want to store the areas
@@ -16,6 +17,9 @@ class MapLogic extends React.Component {
         this.state = {
             areas: []
         }
+
+        this.createAreaList  = this.createAreaList.bind(this);
+        this.waitForHostData = this.waitForHostData.bind(this)
     }
 
     componentDidMount() {
@@ -23,10 +27,46 @@ class MapLogic extends React.Component {
             data => data.json(),
             error => console.error(error)
         ).then(
-            areas => this.setState({
-                areas: areas
+            areas => this.waitForHostData(areas)
+        ).then(
+            areas => this.setState((state,props) => {
+                // eslint-disable-next-line
+                areas: this.createAreaList(areas)
             })
         );
+    }
+
+    // Set an interval check to ensure that the host
+    // data has been fully process from the server
+    // before beginning to construct the area
+    // components within the application.
+    waitForHostData(areas) {
+        return new Promise((resolve, reject) => {
+            var interval;
+
+            function checkHostData(data) {
+                if(data !== undefined) {
+                    clearInterval(interval);
+                }
+
+                resolve(areas);
+            }
+
+            interval = setInterval(() => checkHostData(this.props.hosts), 0);
+        })
+    }
+
+    createAreaList(list) {
+        let areaListArray = [];
+        list.forEach(element => {
+            areaListArray.push(
+                <Area
+                    id = {element.id}
+                    hosts = { this.props.hosts }
+                />
+            )
+        });
+        return areaListArray;
     }
 
     render() {
